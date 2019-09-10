47fb248728a49e6d2f64bf7fbb0ab767a4c2e380
apriliyaard <3103117254@student.smktelkom-pwt.sch.id>
2019-09-10 15:13:28 +0700

    login screen

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import LoginForm from './src/components/LoginForm';
+
+export default class App extends Component{
+  render(){
+    return(
+      <LoginForm />
+    );
+  }
+}
