9237d2eede167a31184d0e60ff8e20cccafa5431
apriliyaard <3103117254@student.smktelkom-pwt.sch.id>
2019-09-11 15:33:43 +0700

    register screen

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import LoginForm from './src/components/LoginForm';
+import RegisterForm from './src/components/RegisterForm';
 
 export default class App extends Component{
   render(){
     return(
-      <LoginForm />
+      <RegisterForm />
     );
   }
 }
